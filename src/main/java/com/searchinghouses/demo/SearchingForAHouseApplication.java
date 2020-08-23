f6e62a8e0ef1a9828b2a699313a17106d1a0d037
abarran03 <58741178+abarran03@users.noreply.github.com>
2020-08-23 16:53:30 -0600

    testing

--- src/main/java/com/searchinghouses/demo/SearchingForAHouseApplication.java
+++ src/main/java/com/searchinghouses/demo/SearchingForAHouseApplication.java
@@ -3,6 +3,7 @@ package com.searchinghouses.demo;
 import com.searchinghouses.house.Aspect;
 import com.searchinghouses.house.House;
 import com.searchinghouses.repository.HouseRepository;
+import com.searchinghouses.service.HouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,37 +18,9 @@ import java.util.UUID;
 @ComponentScan("com.searchinghouses")
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = HouseRepository.class)
-public class SearchingForAHouseApplication  implements CommandLineRunner {
+public class SearchingForAHouseApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchingForAHouseApplication.class, args);
 	}
-	@Autowired
-	private HouseRepository repository;
-
-	@Override
-	public void run(String... args) throws Exception {
-		Aspect aspect1 = new Aspect();
-		aspect1.setDescription("Muebles granito");
-		List<Aspect> aspectList = new ArrayList<>();
-		aspectList.add(aspect1);
-		House h = new House(UUID.randomUUID().toString(), "Casa X");
-		h.setAspects(aspectList);
-
-		// save a couple of customers
-		repository.insert(h);
-
-
-		// fetch all customers
-		System.out.println("House found with findAll():");
-		System.out.println("-------------------------------");
-		for (House house : repository.findAll()) {
-			System.out.println(house);
-		}
-		System.out.println();
-
-
-
-	}
-
 }
